6410f43f7ddc2a90dd52f6013440356d51f4007a
Sengy <marekdud@seznam.cz>
2022-12-19 10:31:50 +0100

    Přepracování visual části
    - Přidán čas vyfocení fotky
    - Upraveno zobrazení celé fotky

--- src/Box.js
+++ src/Box.js
@@ -14,7 +14,7 @@ for(var i = 1; i <= 12; i++){
 };
 
 document.querySelectorAll(".box").forEach((box, index) => {
-    box.style.backgroundPosition=(index * (-800 / items.length)) + "px 0%";
+    box.style.setProperty("--bgpos",(index * (-800 / items.length)) + "px 0%");
     box.style.setProperty("--bright",(0.2 + 0.8 / (index + 1)));
 })
 
@@ -22,28 +22,18 @@ document.querySelectorAll(".box").forEach((box, index) => {
   const [IsOpen, setIsOpen] = useState(false);
   const [openItem, setOpenItem] = useState(null);
 
-  var modal = document.querySelector(".modal");
-  var modalImg = document.querySelector(".modal-img");
 
   const toggleIsOpen = () => {
-    setIsOpen(true);
-    modal.style.display="block";
-    modalImg.src=window.getComputedStyle(document.querySelector(".box")).backgroundImage.slice(5, -2);
+    setIsOpen(current => !current);
   };
-
-  var span = document.getElementsByClassName("close")[0];
-  const modalClose = () => {
-    modal.style.display="none";
-  }
   
 
 return (
   <>
-    <div class="modal"><span class="close" onClick={modalClose}>&times;</span><img class="modal-img"/></div>
     <div class="wrapper">
     <div class="container">
     {items.map(item => (
-        <div key={item} className={"box"} onClick={() => {setOpenItem(item);toggleIsOpen();}}></div>
+        <div key={item} className={`box ${openItem === item && IsOpen  ? 'active' : ''}`} onClick={() => {setOpenItem(item);toggleIsOpen();}}>{item}AM</div>
     ))}
     </div>
     </div>
