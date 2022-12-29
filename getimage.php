da53d8f125494158d17855c9bdb89ab53f308f50
Sengy <marekdud@seznam.cz>
2022-12-29 16:09:33 +0100

    Update getimage.php

--- getimage.php
+++ getimage.php
@@ -4,21 +4,40 @@ use \PDO;
 
 require __DIR__.'/vendor/autoload.php';
 $client = new Client("wss://cam.kitesportcentre.com/gl-cam");
+date_default_timezone_set("Europe/Dublin"); 
 
 $message = $client->receive();
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO images (timestamp,data)
-    VALUES (CURRENT_TIMESTAMP, '$message')";
-    $conn->exec($sql);
-    //
+
+$timestamp = time();
+$sql_timestamp = date("Y-m-d H:i:s",$timestamp);
+$latitude = "53.350140";
+$longitude = "-6.266155";
+
+$sun_info = date_sun_info($timestamp,$latitude,$longitude);
+
+$sunrise = $sun_info['sunrise'];
+$sunset = $sun_info['sunset'];
+
+if ($timestamp >= $sunrise && $timestamp <= $sunset){
+  try {
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $statement = $connection->prepare("INSERT INTO images (timestamp,data) 
+        VALUES (?,?)");
+
+    $statement->execute(array($sql_timestamp,$message));
+
   } catch(PDOException $e) {
     //
   }
 $conn = null;
-
+}
+else{
+    return;
+}
 
 $client->close();
 ?>
