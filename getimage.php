a088f7bf82239f230120788e47aa4407690dbae4
Sengy <marekdud@seznam.cz>
2022-12-29 12:59:30 +0100

    Update getimage.php

--- getimage.php
+++ getimage.php
@@ -7,11 +7,6 @@ $client = new Client("wss://cam.kitesportcentre.com/gl-cam");
 
 $message = $client->receive();
 
-$servername = "mysql.hostnow.cz";
-$username = "sql1317_cam";
-$password = "pH8d0M2i3#lo";
-$dbname = "sql1317_cam";
-
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
