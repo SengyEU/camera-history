2a92f710199797a90355d34aef301c182376af5b
Sengy <marekdud@seznam.cz>
2023-01-10 15:10:05 +0100

    Pridani comments

--- api.php
+++ api.php
@@ -4,12 +4,18 @@ header("Access-Control-Allow-Methods: GET, OPTIONS, POST");
 header("Access-Control-Allow-Headers: *");
 
 
+// Prijmuti datumu z headeru
+
 $data = json_decode(file_get_contents("php://input"),true);
+
+//Pripojeni na databazi s datumem
 
 $conenction = new PDO('mysql:host=mysql.hostnow.cz;dbname='. $DB, $DB, $DBS);
 $query = $conenction->prepare("SELECT * FROM images WHERE DATE(timestamp) = :datum");
 $query->execute([':datum' => $data["datum"]]);
 $rows = $query->fetchAll();
+
+//Output obrazku jako json
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($rows);
--- getimage.php
+++ getimage.php
@@ -6,15 +6,21 @@ use \PDOException;
 require __DIR__.'/vendor/autoload.php';
 date_default_timezone_set("Europe/Dublin"); 
 
+//Nastaveni mista pro zapad/vychod slunce
+
 $timestamp = time();
 $sql_timestamp = date("Y-m-d H:i:s",$timestamp);
 $latitude = "53.350140";
 $longitude = "-6.266155";
 
+//Zjisteni info o vychodu a zapadu slunce
+
 $sun_info = date_sun_info($timestamp,$latitude,$longitude);
 
 $sunrise = $sun_info['sunrise'];
 $sunset = $sun_info['sunset'];
+
+//Pokud je den, napoj se na websocket, stáhni tam jeden obrázek a ulož ho do databáze společně s timestamp a id
 
 if ($timestamp >= $sunrise && $timestamp <= $sunset){
 
