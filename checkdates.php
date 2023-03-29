1b2f83a89077540bbae90d86f7bc380b477fd712
SengyEU <104589954+SengyEU@users.noreply.github.com>
2023-03-29 17:53:19 +0200

    Update checkdates.php

--- checkdates.php
+++ checkdates.php
@@ -1,6 +1,24 @@
 <?php
 require('db.php');
 
-$dt = DateTime::createFromFormat("Y-m-d H:i:s", "2011-07-26 20:05:00");
-echo $dt->format('H');
+$connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $connection->prepare("SELECT timestamp from images");
+$statement->execute();
+
+$rows = $statement->fetchAll();
+
+foreach ($rows as $row) {
+
+    $date1 = DateTime::createFromFormat("Y-m-d H:i:s", $row[0]);
+
+    $date2 = new DateTime(); // current date and time
+    $diff = $date2->diff($date1);
+
+    $days = $diff->days;
+    echo "" . $date1->format('Y-m-d H:i:s'). " bylo před " . $days . " dny.<br>";
+
+}
 ?>
